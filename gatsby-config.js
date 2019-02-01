ea970f5f05d5cd3e5535f4595ebc2f6e225e8191
Taylor <15224340+ttstauss@users.noreply.github.com>
2019-02-01 15:09:01 -0700

    setup netlify cms and adjust pwa info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Level Up Blog`,
     description: `A new blog.`,
-    author: `Yo Momma`
+    author: `Yo Momma`,
+    siteUrl: `https://blissful-mcnulty-6dde65.netlify.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,18 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Level Up Blog`,
+        short_name: `LUTBlog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#524763`,
+        theme_color: `#524763`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -43,6 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`
     },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-netlify` // make sure to put last in the array
   ]
 }
